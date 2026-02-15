92d80357085891ace0d6822effb09538e470909c
Shiv Kiran <bshivkiran120602@gmail.com>
2026-02-15 16:48:06 +0530

    feat(groups): apply native chrome tab groups

---
+++ apps/extension/src/lib/group-applier.js
@@ -0,0 +1,70 @@
+const GROUP_COLORS = [
+  "orange",
+  "blue",
+  "green",
+  "yellow",
+  "purple",
+  "pink",
+  "cyan",
+  "grey",
+  "red"
+];
+
+function colorForGroup(index) {
+  return GROUP_COLORS[index % GROUP_COLORS.length];
+}
+
+function sanitizeIndices(tabIndices, tabCount, alreadyGrouped) {
+  const unique = [];
+  for (const tabIndex of tabIndices) {
+    if (!Number.isInteger(tabIndex)) {
+      continue;
+    }
+    if (tabIndex < 0 || tabIndex >= tabCount) {
+      continue;
+    }
+    if (alreadyGrouped.has(tabIndex)) {
+      continue;
+    }
+    alreadyGrouped.add(tabIndex);
+    unique.push(tabIndex);
+  }
+  return unique;
+}
+
+/**
+ * @param {import("./models").OrganizeRequestTab[]} tabs
+ * @param {import("./models").GroupSuggestion[]} suggestions
+ * @returns {Promise<import("./models").ApplySummary>}
+ */
+export async function applyTabGroups(tabs, suggestions) {
+  const groupedIndices = new Set();
+  let groupsCreated = 0;
+
+  for (const suggestion of suggestions) {
+    const validIndices = sanitizeIndices(
+      suggestion.tabIndices,
+      tabs.length,
+      groupedIndices
+    );
+    if (validIndices.length === 0) {
+      continue;
+    }
+
+    const tabIds = validIndices.map((index) => tabs[index].chromeTabId);
+    const groupId = await chrome.tabs.group({ tabIds });
+
+    await chrome.tabGroups.update(groupId, {
+      title: suggestion.name.slice(0, 40),
+      color: colorForGroup(groupsCreated)
+    });
+
+    groupsCreated += 1;
+  }
+
+  return {
+    groupedTabs: groupedIndices.size,
+    groupsCreated,
+    skippedTabs: tabs.length - groupedIndices.size
+  };
+}
